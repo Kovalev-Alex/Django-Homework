cbf6d0b16c3da14d702ee6e8c0b3d76c642cca47
Alexandr Kovalev <alex_kov@inbox.ru>
2024-03-28 00:52:07 +0500

    Есть вопросы...

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_object = Phone.objects.all()
+    phone_name = (p.name for p in phone_object)
+    phone_price = (p.price for p in phone_object)
+    phone_image = (p.image for p in phone_object)
+    
+    
+    context = {
+        'phones': {
+            'name': phone_name,
+            'price': phone_price,
+            'image': phone_image
+
+        }
+    }
+    
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+
+    }
     return render(request, template, context)
+ 
